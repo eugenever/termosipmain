fcfdd53138f0050af7d279b15144ac30f9a361f7
eugenever <eugenepr@yandex.ru>
2014-03-25 19:55:40 +0400

    ckeditor2

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,7 +42,6 @@ desc 'copy ckeditor nondigest assets'
 task :copy_nondigest_assets, roles: :app do
   run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} ckeditor:create_nondigest_assets"
 end
-
 after 'deploy:assets:precompile', 'copy_nondigest_assets'
 
 # Далее идут правила для перезапуска unicorn. Их стоит просто принять на веру - они работают.
