efead16fed7346101fade8fc8643c20fe98bb34f
eugenever <eugenepr@yandex.ru>
2014-04-03 13:24:36 +0400

    restart unicorn add

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,8 @@
 require 'rvm/capistrano' # Для работы rvm
 require 'bundler/capistrano' # Для работы bundler. При изменении гемов bundler автоматически обновит все гемы на сервере, чтобы они в точности соответствовали гемам разработчика. 
 
+set :rvm_ruby_string, '2.0.0@termosipmain' # Это указание на то, какой Ruby интерпретатор мы будем использовать.
+
 set :application, "termosipmain"
 set :rails_env, "production"
 set :domain, "depuser@termosip.ru" # Это необходимо для деплоя через ssh. Именно ради этого я настоятельно советовал сразу же залить на сервер свой ключ, чтобы не вводить паролей.
@@ -8,12 +10,11 @@ set :deploy_to, "/srv/#{application}"
 set :use_sudo, false
 set :unicorn_conf, "#{deploy_to}/current/config/unicorn.rb"
 set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
+set :unicorn_start_cmd, "(cd #{deploy_to}/current; rvm use #{rvm_ruby_string} do bundle exec unicorn_rails -Dc #{unicorn_conf})"
 set :keep_releases, 3 #число релизов оставляемых после деплоя
 set :shared_children, shared_children + %w{public/ckeditor_assets} #чтобы не удалялись предыдущие ассеты из ckeditor
 ssh_options[:forward_agent] = true
 default_run_options[:pty] = true
-
-set :rvm_ruby_string, '2.0.0@termosipmain' # Это указание на то, какой Ruby интерпретатор мы будем использовать.
 
 set :scm, :git # Используем git. Можно, конечно, использовать что-нибудь другое - svn, например, но общая рекомендация для всех кто не использует git - используйте git. 
 set :repository,  "git@github.com:eugenever/termosipmain.git" # Путь до вашего репозитария. Кстати, забор кода с него происходит уже не от вас, а от сервера, поэтому стоит создать пару rsa ключей на сервере и добавить их в deployment keys в настройках репозитария.
@@ -27,11 +28,6 @@ role :db,  domain, :primary => true
 before 'deploy:setup', 'rvm:install_rvm', 'rvm:install_ruby' # интеграция rvm с capistrano настолько хороша, что при выполнении cap deploy:setup установит себя и указанный в rvm_ruby_string руби.
 
 after 'deploy:update_code', :roles => :app do
-  #run "cd #{release_path}; RAILS_ENV=production rake db:migrate"
-  #run "cd #{release_path}; RAILS_ENV=production rake assets:precompile"
-  #run "cd #{release_path}; rails generate ckeditor:install --orm=active_record --backend=paperclip"
-  #run "ln -s #{shared_path}/public/ckeditor_assets #{release_path}/public/ckeditor_assets"
-  # Здесь для примера вставлен только один конфиг с приватными данными - database.yml. Обычно для таких вещей создают папку /srv/myapp/shared/config и кладут файлы туда. При каждом деплое создаются ссылки на них в нужные места приложения.
   #run "rm -f #{current_release}/config/database.yml"
   #run "ln -s #{deploy_to}/shared/config/database.yml #{current_release}/config/database.yml"
   run " cd #{release_path} && RAILS_ENV=production bundle exec rake sitemap:generate"
@@ -59,6 +55,7 @@ after 'deploy:assets:precompile', 'copy_nondigest_assets'
 namespace :deploy do
   task :restart do
     #run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn_rails -c #{unicorn_conf} -E #{rails_env} -D; fi"
+     run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || #{unicorn_start_cmd}"
   end
   task :start do
     run "cd #{deploy_to}/current && bundle exec unicorn_rails -c #{unicorn_conf} -E #{rails_env} -D"
